490db03ac166cfed9b76d5a3c9c18cbd3cbfd6b4
Nicholas-Nilson <106932989+Nicholas-Nilson@users.noreply.github.com>
2022-06-25 19:32:52 -0400

    add calc to develop (#7)
    * removed my calculator class
    
    * attempting to merge Calculator class with develop branch

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -0,0 +1,67 @@
+package com.zipcodewilmington.scientificcalculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char Operator;
+        double x;
+        x = 0;
+        System.out.println(0);
+        System.out.print("Enter the  number");
+        x=input.nextDouble();
+        System.out.println(x);
+        double y;
+        y = input.nextDouble();
+        System.out.println(y);
+        System.out.println("Enter operator: + , -,*, /,^,√");
+        Operator = input.next().charAt(0);
+        double n;
+        switch (Operator){
+            case '+':
+                n = x + y;
+            System.out.println(n);
+            break;
+            case '-':
+                n = x - y;
+            System.out.println(n);
+            break;
+            case '*':
+                n = x * y;
+                System.out.println(n);
+                break;
+            case '/':
+                if (y != 0) {
+                    n = x / y;
+                System.out.println(n);
+            } else {
+                System.out.println("Error");
+            } break;
+            case '^':
+                n = Math.pow(x,2);
+                System.out.println(n);
+                break;
+
+
+
+            default:
+                System.out.println("Invalid operator");
+
+            }
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
