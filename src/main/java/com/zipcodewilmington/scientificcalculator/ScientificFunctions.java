1b317a785e6052f89fa6a15f24017f1f0e30f469
jd <j.judd.singer@gmail>
2022-06-24 22:17:32 -0400

    added scientific function class and created method stubs

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificFunctions.java
@@ -0,0 +1,58 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class ScientificFunctions {
+
+
+//    this is the constructor
+    public ScientificFunctions(){
+
+    }
+
+    public double square(double value){
+        return 0.00;
+    }
+
+    public double squareRoot(double value){
+        return Math.sqrt(value);
+
+    }
+
+    public double inverse(double value){
+        return 0.00;
+    }
+
+    public double switchSign(double value){
+        return 0.00;
+    }
+
+    public double sine(double value){
+        return Math.asin(value);
+
+    }
+
+    public double cosine(double value){
+        return  Math.acos(value);
+    }
+
+    public double tangent(double value){
+        return Math.atan(value);
+    }
+
+    public double inverseSine(double value){
+        return 0.00;
+    }
+
+    public double inverseCosine(double value){
+        return 0.00;
+    }
+
+    public double inverseTangent(double value){
+        return 0.00;
+    }
+
+    public double factorial(double value, double value2){
+        return 0.00;
+    }
+
+
+}
