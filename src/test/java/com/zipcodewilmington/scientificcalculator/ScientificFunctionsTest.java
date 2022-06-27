1d720f39dc0ba0380e7781884ae5c8df6fedffe4
Nick Nilson <nilson.nicholas@gmail.com>
2022-06-26 20:25:54 -0400

    ScientificFunctionsTest update

--- src/test/java/com/zipcodewilmington/scientificcalculator/ScientificFunctionsTest.java
+++ src/test/java/com/zipcodewilmington/scientificcalculator/ScientificFunctionsTest.java
@@ -35,4 +35,19 @@ public class ScientificFunctionsTest {
         Assertions.assertEquals(1.0471975511965979, testCalc.inverseCosine(0.5));
     }
 
+    @Test
+    public void inverseTangentTest() {Assertions.assertEquals(0.4636476090008061, testCalc.inverseTangent(0.5));}
+
+    @Test
+    public void factorialTest() {Assertions.assertEquals(120, testCalc.factorial(5));}
+
+    @Test
+    public void naturalLogTest() {Assertions.assertEquals(3.912023005428146, testCalc.naturalLog(50));}
+
+//    @Test //Inverse log formula not calculating correctly.
+//    public void inverseLogTest() {Assertions.assertEquals(1.6989700043360187, testCalc.inverseLog(50));}
+
+//    @Test //going back to college to learn algebra.
+//    public void inverseLogTest() {Assertions.assertEquals(0, testCalc.inverseNatLog(5));}
 }
+
